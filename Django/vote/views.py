5f7f903e00400d31a716a8a987c6e47da3405d5f
FrozenThrone-YH <52614968+FrozenThrone-YH@users.noreply.github.com>
2019-07-21 10:56:47 +0900

    Update views.py
    views.py를 작성했으면 return render로 보내줄 vote/detail.html을 만들어줄 차례!

--- Django/vote/views.py
+++ Django/vote/views.py
@@ -16,5 +16,20 @@ def main(request):
     #추출된 Question 객체를 HTML 편집에 사용할 수 있도록 전달
     return render(request,"vote/main.html",{'q':q})
 
+#웹 클라이언트가 요청한 Question객체 한개와 연결된 choice객체 추출 
+#q_id : 웹클라이언트가 요청한 Question 객체의 id변수 값 
+def detail(request,q_id):
+    #Question 객체를 한개 추출 - id변수값이 q_id와 같은 조건에 맞는
+    q = Question.objects.get(id=q_id)
+    #추출한 Question객체와 연동된 Choice객체들을 추출
+    #외래키로 연결된 Question객체가 Choice객체들을 대상으로 
+    #추출함수를 사용하려면 객체.choice_set.추출함수로 추출
+    #외래키로 연결된 객체.외래키로 연결한 모델클래스명_set.all,get 
+    c = q.choice_set.all()
+    print(q)
+    print(c)
+    #HTML코드로 추출한 객체들 전달 
+    return render(request,"vote/detail.html",{'q':q,'c':c})
+
 
 
