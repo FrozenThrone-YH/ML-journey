bcbb5bec3d3395ba91f2ad93fedc86a78762981c
FrozenThrone-YH <52614968+FrozenThrone-YH@users.noreply.github.com>
2020-06-19 11:17:58 +0900

    Create 04.MainActivity.java

---
+++ Android/30.FlagActivity/04.MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myflagactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn_next,btn_exit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_next = (Button)findViewById()
+
+    }
+}
