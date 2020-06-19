1ed6ee9bb1b1e983b50491fbd4edefce47356b1f
FrozenThrone-YH <52614968+FrozenThrone-YH@users.noreply.github.com>
2020-06-19 22:24:52 +0900

    Update 04.MainActivity.java

--- Android/30.FlagActivity/04.MainActivity.java
+++ Android/30.FlagActivity/04.MainActivity.java
@@ -2,7 +2,9 @@ package com.example.myflagactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,7 +16,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_next = (Button)findViewById()
+        btn_next = (Button)findViewById(R.id.btn_next);
+        btn_exit = (Button)findViewById(R.id.btn_exit);
 
+        btn_next.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,subactivity.class);
+
+                //페이지가 두개일때 둘중에 하나만 써도 된다.
+//                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+                startActivity(intent);
+            }
+        });
+
+        btn_exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.exit(0);
+            }
+        });
     }
 }
