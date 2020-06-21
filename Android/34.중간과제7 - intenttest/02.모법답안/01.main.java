b054503f3654f6deaa8f1a5d05f83b4a85eed8ba
FrozenThrone-YH <52614968+FrozenThrone-YH@users.noreply.github.com>
2020-06-22 07:45:08 +0900

    Create 01.main.java

---
+++ Android/34.중간과제7 - intenttest/02.모법답안/01.main.java
@@ -0,0 +1,14 @@
+package com.example.myintenttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
