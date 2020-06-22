454da3640b5ea341bda1c786612e85383abaf023
FrozenThrone-YH <52614968+FrozenThrone-YH@users.noreply.github.com>
2020-06-23 08:07:29 +0900

    Update 01.main.java

--- Android/37.Canvas/03.펜으로 그림그리기/01.main.java
+++ Android/37.Canvas/03.펜으로 그림그리기/01.main.java
@@ -1,53 +1,92 @@
 package com.example.mycanvasactivity;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.view.MotionEvent;
+import android.view.View;
 
-import android.content.res.Configuration;
-import android.content.res.Resources;
-import android.os.Bundle;
-import android.widget.Toast;
+public class MyView extends View {
+    private Paint paint = new Paint();
 
-//그림을 그리는 Canvas, Paint 객체를 사용하여 그려보자..
+    //그림을 그리기 위해서 필욯나 붗과 같은 객체
+    //Paint
+    private Path path = new Path();
+    private int x,y;
 
-public class MainActivity extends AppCompatActivity {
+    public MyView(Context context){
+        super(context);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
+    protected void onDraw(Canvas canvas) {
+        paint.setColor(Color.BLACK);
 
-        setContentView(new MyView(this));
+        //STROKE 속성을 이용하여 테두리...선...
+        paint.setStyle(Paint.Style.STROKE);
 
-        //디바이스 회전시 값 유지를 위한 코드
-        //리소스 시스템을 받아서 리소스안에다가 객체를 넣어준다.
-        Resources r = Resources.getSystem();
-        Configuration config = r.getConfiguration();
-        onConfigurationChanged(config);
+        //두께
+        paint.setStrokeWidth(3);
+
+        //path객체가 가지고 있는 경로를 화면에 그린다.
+        canvas.drawPath(path,paint);
+
+        //그림을 그리는 메서드
+        super.onDraw(canvas);
+
+        //그림을 그리기 위해서 필요한 붗과 같은 객체
+        //Paint
+        Paint paint = new Paint();
+
+        //Paint 객체에 색상 정하기
+        paint.setColor(Color.BLUE);
+
+        //캔버스에 그림을 그린다.
+        //사각형 범위 지정
+        //사각형(left,top,right,bottom, paint 객체
+        //숫자의 의미 왼쪽 상단 모서리 기준으로 left 마진이 100, top 위가 200
+        //그리고자 하는 오른쪽 하단 기준으로 bottom margin이 parent 위쪽부터 400
+        //그리고자 하는 오른쪽 하단 기준으로 right margint은 parent 왼쪽 기준으로부터 300
+//        canvas.drawRect(100,200,300,400,paint);
+
+        //원(x축,y축, 반지름, paint객체)
+        //원의 중심점 기준으로 left와 top으로부터 margin, 반지름
+//        canvas.drawCircle(200,600,100,paint);
+
+//        paint.setColor(Color.RED);
+
+//        canvas.drawRect(100,100,200,200,paint);
+
+        //테두리만 있는 상자
+//        paint.setStyle(Paint.Style.STROKE);
+//        paint.setColor(Color.GREEN);
+//        canvas.drawRect(200,100,300,200,paint);
+
 
     }
-    //onConfigurationChanged
-    //오버라이드를 했을 경우 회전시마다 onCreate메서드를 호출하므로
-    //재호출하지 않도록 manifest에 속성두가지를 추가해주면 된다.
-//            <activity android:name=".MainActivity"
-//    android:configChanges="orientation|keyboardHidden|screenSize"
-//    android:windowSoftInputMode="stateHidden|adjustPan">
-    //Activity가 여러개라면 모든 액티비티에 속성을 추가해주는 것이 좋다.
-    @Override
-    public void onConfigurationChanged(@NonNull Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
 
-        //현재 디바이스의 방향성을 체크
-        switch(newConfig.orientation){
-            case Configuration.ORIENTATION_LANDSCAPE:
-                Toast.makeText(getApplicationContext(),"가로",Toast.LENGTH_SHORT).show();
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        x = (int)event.getX();
+        y = (int)event.getY();
+
+        switch(event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                //x,y대로 움직인다.
+                path.moveTo(x,y);
                 break;
-            case Configuration.ORIENTATION_PORTRAIT:
-                Toast.makeText(getApplicationContext(),"세로",Toast.LENGTH_SHORT).show();
+            case MotionEvent.ACTION_MOVE:
+                x = (int)event.getX();
+                y = (int)event.getY();
+                path.lineTo(x,y);
                 break;
-                //화면이 돌아갈때마다 그림을 그린게 사라진다.
-                //그것은 화면이 돌아갈때마다 재호출을 해서 그렇다.
-                //속성값을 추가해서 유지하고자 하낟.
+            //터치에서 손을 떼는 것은 정의안함
         }
+
+        //View의 onDraw()를 호출하는 메서드
+        invalidate();
+        return true;
     }
 }
